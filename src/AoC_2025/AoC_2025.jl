54318e23e64f10e7c0e27d80cbfe0402a9554893
Titas22 <66130422+Titas22@users.noreply.github.com>
2025-11-15 16:46:55 +0000

    chore: update fore 2025

---
+++ src/AoC_2025/AoC_2025.jl
@@ -0,0 +1,36 @@
+module AoC_2025
+    using AdventOfCode
+    const AoC = AdventOfCode
+    
+    
+    # file    = @__FILE__;
+    
+    # # folder     = @__DIR__;
+
+    # (folder, filename)  = splitdir(file)
+    # (fname, ext)        = splitext(filename)
+
+    # println("Filename '$(filename)'   Extension: '$(ext)'")
+
+
+    # files = readdir(folder, join=true)
+    # println(folder)
+    # println(file)
+
+    # days        = AoC.DEFAULT_SOLVED_DAYS
+
+    # allFiles    = map(x->joinpath(folder, "$(fname)_$(lpad(x,2,"0"))$(ext)"), days)
+    # solvedFiles = intersect(allFiles, files)
+
+
+
+
+
+
+
+    # TODO: Add Benchmarks here
+
+    # include("./AoC_2025_01.jl")
+    # # import  ..AoC_2025_01;
+    # display(@benchmark (part1, part2) = AoC_2025_01.solve());
+end 

f91673895d8f866605d55f5505dc309da95ef380
Titas Bučelis <66130422+Titas22@users.noreply.github.com>
2025-12-04 00:43:37 +0000

    chore: set up empty 2025 day 4 solution

---
+++ src/AoC_2025/AoC_2025_04.jl
@@ -0,0 +1,41 @@
+module AoC_2025_04
+    using AdventOfCode
+    const AoC = AdventOfCode
+
+    function parse_inputs(lines::Vector{String})
+
+        return lines
+    end
+    function solve_common(inputs)
+
+        return inputs
+    end
+
+    function solve_part_1(inputs)
+
+        return nothing
+    end
+
+    function solve_part_2(inputs)
+
+        return nothing
+    end
+
+    function solve(btest::Bool = false; use_input_cache::Bool = false)::Tuple{Any, Any}
+        lines  = @getinputs(btest, "", use_input_cache)
+        # lines2      = @getinputs(btest, "_2") # Use if 2nd problem test case inputs are different
+        inputs      = parse_inputs(lines)
+
+        solution    = solve_common(inputs)
+        part1       = solve_part_1(solution)
+        part2       = solve_part_2(solution)
+
+        return (part1, part2);
+    end
+
+    @time (part1, part2) = solve(true) # Test
+    # @time (part1, part2) = solve()
+    println("\nPart 1 answer: $(part1)")
+    println("\nPart 2 answer: $(part2)\n")
+end
+# lines = @getinputs(true)

25f0ec3e068ef7c2ff1accaa56b351885e796bec
Titas Bučelis <66130422+Titas22@users.noreply.github.com>
2025-12-06 14:15:57 +0000

    feat: dirty 2025 day 6 part 2 solution

--- src/AoC_2025/AoC_2025_06.jl
+++ src/AoC_2025/AoC_2025_06.jl
@@ -40,6 +40,7 @@ module AoC_2025_06
 end
 
 # 4722948564882
+# 9581313737063
 lines = @getinputs(false)
 
 inputs = (line->parse_int_ascii.(split(strip(line), r"\s+"))).(lines[1:end-1])
@@ -66,3 +67,49 @@ for idx in eachindex(ops)
     println(terms)
 end
 tot
+
+
+
+op_str = pop!(lines)
+
+n_ops = length(op_str) - count(x->x==' ', op_str)
+
+chmat = lines2charmat(lines)
+
+tot2 = 0
+
+n = length(lines)
+terms = zeros(Int64, 4)
+tot2 = 0
+b_multiply = false
+op_offset = 0
+
+for idx in eachindex(op_str)
+    global tot2, op_str, terms, b_multiply, chmat, op_offset
+    op = op_str[idx]
+
+    if op != ' '
+        last_term = idx - op_offset - 2
+        println("idx: $idx   op_offset $op_offset   last_term: $last_term")
+        println("Calculating: $(string(terms)) -> $(b_multiply ? prod(terms[1:last_term]) : sum(terms[1:last_term]))")
+        tot2 += b_multiply ? prod(terms[1:last_term]) : sum(terms[1:last_term])
+        op_offset = idx - 1
+        b_multiply = op == '*'   
+        terms .= 0
+    end
+
+    idx_cur = idx - op_offset
+    for ii in 1 : n
+        ch = chmat[ii, idx]
+        ch == ' ' && continue
+        terms[idx_cur] *= 10
+        terms[idx_cur] += ch - '0'
+    end
+
+    println("Terms: $(string(terms))")
+end
+
+last_term = length(op_str) - op_offset
+println("idx: $(length(op_str)+1)   op_offset $op_offset   last_term: $last_term")
+println("Calculating: $(string(terms)) -> $(b_multiply ? prod(terms[1:last_term]) : sum(terms[1:last_term]))")
+tot2 += b_multiply ? prod(terms[1:last_term]) : sum(terms[1:last_term])
